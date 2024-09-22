e932695dc8ba52052df176517d7e794ef9bfbd32
unreall3r <unreall3r@gmail.com>
2024-09-22 15:41:03 +0300

    First task is ready

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,23 @@
-"use strict"
+"use strict";
+
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let discriminant = b ** 2 - 4 * a * c;
+
+  if (discriminant < 0) {
+    return arr;
+  } else if (discriminant === 0) {
+    let root = -b / (2 * a);
+    arr.push(root);
+  } else {
+    let root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+    let root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+    arr.push(root1, root2);
+  }
+
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+
 }

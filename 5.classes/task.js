3744df8967fff17f29144092282453ab9f25f246
unreall3r <unreall3r@gmail.com>
2025-01-19 12:58:05 +0300

    второй коммит исправление ошибок

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -27,6 +27,19 @@ class PrintEditionItem {
       return this._state;
     }
   }
+
+
+
+  const sherlock = new PrintEditionItem(
+    "Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе",
+    2019,
+    1008
+   );
+   
+   console.log(sherlock.releaseDate); //2019
+   console.log(sherlock.state); //100
+   sherlock.fix();
+   console.log(sherlock.state); //100
   
   class Magazine extends PrintEditionItem {
     constructor(name, releaseDate, pagesCount) {
@@ -35,6 +48,8 @@ class PrintEditionItem {
     }
   }
   
+
+
   class Book extends PrintEditionItem {
     constructor(author, name, releaseDate, pagesCount) {
       super(name, releaseDate, pagesCount);
@@ -63,3 +78,18 @@ class PrintEditionItem {
       this.type = "detective";
     }
   }
+
+
+
+  const picknick = new FantasticBook(
+    "Аркадий и Борис Стругацкие",
+    "Пикник на обочине",
+    1972,
+    168
+  );
+  
+  console.log(picknick.author); //"Аркадий и Борис Стругацкие"
+  picknick.state = 10;
+  console.log(picknick.state); //10
+  picknick.fix();
+  console.log(picknick.state); //15

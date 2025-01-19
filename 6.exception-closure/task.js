4859dc16d530939ed55530a5b44909d3eda0d2e9
unreall3r <unreall3r@gmail.com>
2025-01-19 20:09:11 +0300

    Решение второй задачи

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -14,4 +14,41 @@
     }
   }
 
+  //Вторая задача
+
+class Triangle {
+    constructor(a, b, c) {
+      if (a + b <= c || a + c <= b || b + c <= a) {
+        throw new Error("Треугольник с такими сторонами не существует");
+      }
+      this.a = a;
+      this.b = b;
+      this.c = c;
+    }
+  
+    get perimeter() {
+      return this.a + this.b + this.c;
+    }
+  
+    get area() {
+      const p = this.perimeter / 2;
+      const area = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
+      return area.toFixed(3);
+    }
+  }
+  
+  function getTriangle(a, b, c) {
+    try {
+      return new Triangle(a, b, c);
+    } catch (error) {
+      return {
+        get area() {
+          return "Ошибка! Треугольник не существует";
+        },
+        get perimeter() {
+          return "Ошибка! Треугольник не существует";
+        }
+      };
+    }
+  }
   

9d63e26d30665aa45aba8f904671630f3d963a09
unreall3r <unreall3r@gmail.com>
2025-01-19 20:00:02 +0300

    Задание готово

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,16 @@
-﻿
+﻿function parseCount(value) {
+    const parsedValue = Number.parseFloat(value);
+    if (isNaN(parsedValue)) {
+      throw new Error("Невалидное значение");
+    }
+    return parsedValue;
+  }
+  
+  function validateCount(value) {
+    try {
+      return parseCount(value);
+    } catch (error) {
+      return error.message;
+    }
+  }
+  
